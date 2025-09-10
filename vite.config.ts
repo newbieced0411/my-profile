72005cbe04342e5b3e135c16a6fa4a56c069a9b1
newbieced0411 <jctdelacarcel@outlook.com>
2025-09-10 16:04:09 +0800

    fix: set base path to root for all environments

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/my-profile/' : '/',
+  base: '/',
 })
